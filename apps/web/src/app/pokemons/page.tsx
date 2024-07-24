4aa28eb85bd0584274bdaeb6e6fa09a218cd8a3f
CNR <narayana.cheerla@gmail.com>
2024-07-24 14:09:54 +0530

    tanstack packages added

---
+++ apps/web/src/app/pokemons/page.tsx
@@ -0,0 +1,13 @@
+const Pokemons = () => {
+  return (
+    <div className="flex items-center flex-col">
+      <h1>Pokemons List</h1>
+      <ul>
+        <li>One</li>
+        <li>Two</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Pokemons;
